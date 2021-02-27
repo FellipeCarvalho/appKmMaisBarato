75c1cdd69f8645d77b0d6e36986ed4f98030e8e8
Fellipe Carvalho <fellipealbert3@gmail.com>
2021-02-27 09:55:43 -0300

    appMaisBarato prototipo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,38 @@ export class HomePage {
 
   constructor() {}
 
+
+  fechar(){
+    navigator['app'].exitApp();
+    
+  }
+
+  public precoGasolina: string;
+  public precoAlcool: string; 
+  public resultadoTexto: String = "  Preencha os valores para que seja calculado qual opção se encaixa mais no seu bolso.  "
+
+  calculaPreco(){
+    if( this.precoGasolina && this.precoAlcool ) {
+
+        //para fazer o calculo tem que fazer o parse no angular
+        var pAlcool = parseFloat(this.precoAlcool)
+        var pGas = parseFloat(this.precoGasolina)
+
+        var res = pAlcool /  pGas 
+        
+        if(res >= 0.7 ){
+          this.resultadoTexto = " A gasolina vale mais a pena! "
+        }else{
+          this.resultadoTexto = " O álcool vale mais a pena!  "
+        }
+    }
+
+
+
+
+  }
+
+
+  
+
 }
